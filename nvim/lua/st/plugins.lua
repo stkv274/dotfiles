a57e088b8bf0e5353954e0e8216b2a3bd2469d81
stkv274 <st.kv274@gmail.com>
2023-06-16 10:23:58 +0200

    Add tabby plugin

--- nvim/lua/st/plugins.lua
+++ nvim/lua/st/plugins.lua
@@ -129,6 +129,9 @@ return packer.startup(function(use)
     requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
   }
 
+  -- Tabs
+   use "nanozuki/tabby.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
